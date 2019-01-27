54e27a76f1a0a6079ed7b0d4fcb95b2f7492aa79
jason <gitcommit@fragstealers.com>
2019-01-27 13:12:27 +1000

    Store LE config locally and tar/untar as required to make it more friendly with Azure Container Instances

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -10,16 +10,10 @@ echo "Running certbot renew"
 if [ "$ORIGVALIDATION" = "dns" ] || [ "$ORIGVALIDATION" = "duckdns" ]; then
   certbot -n renew \
     --post-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -h /var/run/s6/services/nginx; fi; \
-    cd /config/keys/letsencrypt && \
-    openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass: && \
-    sleep 1 && \
-    cat privkey.pem fullchain.pem > priv-fullchain-bundle.pem"
+    tar -czf /config/etc/letsencrypt/letsencrypt-config.tar.gz -C / etc/letsencrypt/"
 else
   certbot -n renew \
     --pre-hook "if ps aux | grep [n]ginx: > /dev/null; then s6-svc -d /var/run/s6/services/nginx; fi" \
     --post-hook "if ps aux | grep 's6-supervise nginx' | grep -v grep > /dev/null; then s6-svc -u /var/run/s6/services/nginx; fi; \
-    cd /config/keys/letsencrypt && \
-    openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass: && \
-    sleep 1 && \
-    cat privkey.pem fullchain.pem > priv-fullchain-bundle.pem"
+    tar -czf /config/etc/letsencrypt/letsencrypt-config.tar.gz -C / etc/letsencrypt/"
 fi
